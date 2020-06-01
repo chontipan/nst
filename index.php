4066b2d8e94e620ad83a7b09477d921814ee953e
Chontipan Plengvittaya <chontipan@gmail.com>
2020-06-01 16:45:38 +0800

    edit path refer

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="/style-transfer/style.css" />
+    <link rel="stylesheet" href="/style.css" />
 	
-    <script src="/style-transfer/jquery.min.js"></script>
-    <script src="/style-transfer/magentaimage.min.js"></script>
+    <script src="/jquery.min.js"></script>
+    <script src="/magentaimage.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js"> </script>
     
     <script src="/style-transfer/script.js" defer></script>
